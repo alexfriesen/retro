f88f44b7bb8612f05e8c51889c0e9af8e8bd7c31
Alexander Friesen <friesen.alex.home@gmail.com>
2023-03-30 17:28:07 +0200

    🔧 improve typescript config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-import { defineConfig } from "vite";
-import glsl from "vite-plugin-glsl";
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [
-    glsl({
-      include: [
-        // Glob pattern, or array of glob patterns to import
-        "**/*.glsl",
-        "**/*.wgsl",
-        "**/*.vert",
-        "**/*.frag",
-        "**/*.vs",
-        "**/*.fs",
-      ],
-      compress: true,
-    }),
-  ],
-  // config options
-  base: "/retro/",
-  build: {
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
+	plugins: [
+		glsl({
+			include: [
+				// Glob pattern, or array of glob patterns to import
+				'**/*.glsl',
+				'**/*.wgsl',
+				'**/*.vert',
+				'**/*.frag',
+				'**/*.vs',
+				'**/*.fs',
+			],
+			compress: true,
+		}),
+	],
+	// config options
+	base: '/retro/',
+	build: {
+		chunkSizeWarningLimit: 500,
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name].js`,
+				chunkFileNames: `[name].js`,
+				assetFileNames: `[name].[ext]`,
+			},
+		},
+	},
 });
